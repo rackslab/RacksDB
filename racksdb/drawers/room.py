5ab6122ef8e7a983f980e91c75f51ead5090c032
Rémi Palancher <remi@rackslab.io>
2022-10-03 16:22:22 +0200

    feat(draw): honor racks rows rotation in rooms

--- racksdb/drawers/room.py
+++ racksdb/drawers/room.py
@@ -52,7 +52,9 @@ class RoomDrawer(Drawer):
         )
 
     def _rack_tl(self, rack):
-        tl = self._racks_row_tl(rack.row)
+        # Start from top-left corner of the rack row, which is (0,0) after
+        # context matrix translation.
+        tl = ImagePoint(0, 0)
         # Sum the width of all racks in row before the current rack
         filled_slots = {}
         for row_rack in rack.row.racks:
@@ -111,9 +113,18 @@ class RoomDrawer(Drawer):
         # write rack name
         self.ctx.set_source_rgb(0, 0, 0)  # black
         if rack_height > rack_width:
-            self.ctx.move_to(tl.x + 2, tl.y + 2)
             self.ctx.save()
-            self.ctx.rotate(math.pi / 2)
+            # If the rack row rotation angle is between 0 and 180, flip the text
+            # to be right side up.
+            if (
+                rack.row.position.rotation > 0
+                and rack.row.position.rotation < 180
+            ):
+                self.ctx.move_to(tl.x + 15, tl.y - 2 + rack_height)
+                self.ctx.rotate(-math.pi / 2)
+            else:
+                self.ctx.move_to(tl.x + 2, tl.y + 2)
+                self.ctx.rotate(math.pi / 2)
         else:
             self.ctx.move_to(tl.x + 2, tl.y + 15)
         self.ctx.show_text(rack.name)
@@ -155,6 +166,13 @@ class RoomDrawer(Drawer):
         )
 
         for row in self.room.rows:
+            # Translate the context matrix to rack row top-left corner and
+            # rotate it as per row rotation angle.
+            self.ctx.save()
+            tl = self._racks_row_tl(row)
+            self.ctx.translate(tl.x, tl.y)
+            self.ctx.rotate(row.position.rotation * math.pi / 180)
             self._draw_racks_row(row)
+            self.ctx.restore()
 
         self.write()
