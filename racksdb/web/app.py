34c168ee43cec9cdf78a4e4ce5898823d8571869
Florian Lespérant <florian.lesperant@rackslab.io>
2023-08-03 10:40:37 +0200

    fix: reorganization of the back, one route takes care of one page

--- racksdb/web/app.py
+++ racksdb/web/app.py
@@ -6,9 +6,11 @@ app = Flask(__name__)
 CORS(app) # Permet d'activer CORS pour que le frontend Vue.js puisse accéder au backend
 db = RacksDB.load()
 
-@app.route('/api/datacenters', methods=['GET'])
-def get_datacenters():
+# This route gives all the data (datacenter and infrastructure) for the page homeview
+@app.route('/api/homeview', methods=['GET'])
+def get_homeview():
     datacenters = []
+    infrastructures = []
     for datacenter in db.datacenters.items:
         for room in datacenter.rooms:
             datacenter_info ={
@@ -18,26 +20,40 @@ def get_datacenters():
                 'room_width': room.dimensions.width,
                 'room_depth': room.dimensions.depth,
                 'nb_rack': len(room.rows.items),
-
             }
             datacenters.append(datacenter_info)
-    return jsonify(datacenters)
-
-@app.route('/api/infrastructures', methods=['GET'])
-def get_infrastructures():
-    infrastructures = []
 
     for infrastructure in db.infrastructures:
         infrastructure_info = {
             'name': infrastructure.name,
-            'tags': infrastructure.tags
+            'tags': infrastructure.tags,
         }
         infrastructures.append(infrastructure_info)
-    return jsonify(infrastructures)
 
-@app.route('/api/racks', methods=['GET'])
-def get_racks():
+    reponse_data = {
+        'datacenters': datacenters,
+        'infrastructures': infrastructures
+    }
+    return jsonify(reponse_data)
+
+
+# This route gives all the data for the page datacenter
+@app.route('/api/datacenters', methods=['GET'])
+def get_datacenters():
+    datacenters = []
     racks = []
+    for datacenter in db.datacenters.items:
+        for room in datacenter.rooms:
+            datacenter_info ={
+                'name': datacenter.name,
+                'tags': datacenter.tags,
+                'room_name': room.name,
+                'room_width': room.dimensions.width,
+                'room_depth': room.dimensions.depth,
+                'nb_rack': len(room.rows.items),
+            }
+            datacenters.append(datacenter_info)
+
     for datacenter in db.datacenters.items:
         for room in datacenter.rooms:
             for row in room.rows:
@@ -47,7 +63,44 @@ def get_racks():
                         'rack_name': rack.name,
                     }
                     racks.append(racks_info)
-    return jsonify(racks)
+
+    reponse_data = {
+        'datacenters': datacenters,
+        'racks': racks
+    }
+    return jsonify(reponse_data)
+
+
+
+# This route gives all the data for the page infrastructure
+@app.route('/api/infrastructures', methods=['GET'])
+def get_infrastructures():
+    infrastructures = []
+    racks = []
+    for infrastructure in db.infrastructures:
+                infrastructure_info = {
+                    'name': infrastructure.name,
+                    'tags': infrastructure.tags,
+                }
+                infrastructures.append(infrastructure_info)
+
+    for infrastructure in db.infrastructures:
+        for rack in infrastructure.layout:
+             for node in rack.nodes:
+                  rack_info = {
+                       'infrastructure_name': infrastructure.name,
+                       'rack_name': rack.rack.name,
+                       'node_name': node.name,
+                       'node_id': node.type.id,  
+                  }
+                  racks.append(rack_info)
+
+
+    reponse_data = {
+        'infrastructures': infrastructures,
+        'racks': racks
+    }
+    return jsonify(reponse_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
