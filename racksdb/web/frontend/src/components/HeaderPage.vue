68c7a3224571d981da33f1a21d0a7924758ae99a
Florian Lespérant <florian.lesperant@rackslab.io>
2023-08-05 16:21:02 +0200

    fix: adding comments to the code

--- racksdb/web/frontend/src/components/HeaderPage.vue
+++ racksdb/web/frontend/src/components/HeaderPage.vue
@@ -2,40 +2,40 @@
     <div class="header">
       <nav class="navbar navbar-expand-lg">
         <div class="container-fluid">
-          <a class="navbar-brand" href="/">
-            <img src="../assets/racksdb_logo.svg" alt="Logo Racksdb" width="100" height="100">
-          </a>
+
+            <!-- Left part of the navbar : clickable logo -->
+            <a class="navbar-brand" href="/">
+              <img src="../assets/racksdb_logo.svg" alt="Logo Racksdb" width="100" height="100">
+            </a>
   
+          <!-- Lateral menu for small screen -->
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
   
+          <!-- Right part of the navbar : different pages of the site -->
           <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
             <ul class="navbar-nav">
               <router-link to="/" exact class="nav-link" :class="{ 'active': $route.path === '/' }"><li class="nav-item">Home</li></router-link>
               <router-link to="/datacenters" class="nav-link" :class="{ 'active': $route.path === '/datacenters' }"><li class="nav-item">Datacenters</li></router-link>
               <router-link to="/infrastructures" class="nav-link" :class="{ 'active': $route.path === '/infrastructures' }"><li class="nav-item">Infrastructures</li></router-link>
             </ul>
-
-
           </div>
+
         </div>
       </nav>
       <router-view/>
+
     </div>
 </template>
-  
-
-  
+    
 <script>
 export default {
   name: 'HeaderPage'
 }
-
 </script>
   
 <style scoped src="../assets/css/HeaderPage.css">
-
 </style>
   
