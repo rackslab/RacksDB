6f0e616bb76911dac8bb2f6aaa7fc0fc4a8e0193
Florian Lespérant <florian.lesperant@rackslab.io>
2023-08-21 09:02:32 +0200

    fix: remove all unnecessary code and comments

--- racksdb/web/frontend/src/views/HomeView.vue
+++ racksdb/web/frontend/src/views/HomeView.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="home">
 
-      <!-- C'est la partie du site ou il y a la bannière (effet de superposition entre img, filtre couleur et texte)-->
       <section class="slider">
         <div class="overlay"></div>
         <div class="main-title">
@@ -9,35 +8,34 @@
         </div>
       </section>
 
-      <!-- Les cartes qui affichent les informations de la base de données-->
       <div class="cards">
-
         <div class="datacenters">
           <router-link to="/datacenters"><h2 class="title_card">{{datacenters.length}} Datacenters</h2></router-link> 
+          
             <ul>
               <li v-for="datacenter in datacenters" :key="datacenter.id">
-                <router-link :to="getDatacenterDetailsRoute(datacenter.name)"><span class="name">{{datacenter.name}}</span></router-link> <span class="description">, {{ datacenter.tags.join(' ') }}</span>
+                <router-link :to="getDatacenterDetailsRoute(datacenter.name)"><span class="name">{{datacenter.name}}</span></router-link> 
+                  <span class="description">, {{ datacenter.tags.join(' ') }}</span>
               </li>
             </ul>
         </div>
 
         <div class="infrastructures">
           <router-link to="/infrastructures"><h2 class="title_card">{{ infrastructures.length }} Infrastructures</h2></router-link>
+
             <ul>
               <li v-for="infrastructure in infrastructures" :key="infrastructure.id">
-                <router-link :to="getInfrastructureDetailsRoute(infrastructure.name)"><span class="name">{{infrastructure.name}}</span></router-link> <span class="description">, {{ infrastructure.tags.join(' ') }}</span>
+                <router-link :to="getInfrastructureDetailsRoute(infrastructure.name)"><span class="name">{{infrastructure.name}}</span></router-link>
+                  <span class="description">, {{ infrastructure.tags.join(' ') }}</span>
               </li>
             </ul>
         </div>
+
       </div>
-
-
-
     </div>
 </template>
     
 <script>
-// Axio est une librairie qui permet de faire des requêtes plus simplement et de se protéger des failles XSRF
 import axios from 'axios';
 
 export default {
@@ -45,12 +43,13 @@ export default {
     return {
       datacenters: [],
       infrastructures: [],
-
     };
   },
+
   mounted() {
     this.fetchData();
   },
+
   methods: {
     async fetchData() {
       try {
@@ -64,20 +63,15 @@ export default {
       }
     },
 
-
-    // This method send the user to a dedicated page for the datacenter selected
     getDatacenterDetailsRoute(datacenterName) {
       return `/datacenters/${encodeURIComponent(datacenterName)}`
     },
 
-    // This method send the user to a dedicated page for the datacenter selected
     getInfrastructureDetailsRoute(infrastructureName) {
       return `/infrastructures/${encodeURIComponent(infrastructureName)}`
     },
-
   },
 };
-
 </script>
 
 <style scoped src="../assets/css/HomeView.css">
