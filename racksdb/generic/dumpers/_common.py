a6fd21deaebbd474ca1cdd86681f55e2904b4256
Rémi Palancher <remi@rackslab.io>
2023-09-26 12:19:18 +0200

    feat(core): support map of objects properties
    In MapperDumper, support map of objects properties in addition to the
    objects themselves.

--- racksdb/generic/dumpers/_common.py
+++ racksdb/generic/dumpers/_common.py
@@ -12,8 +12,14 @@ class MapperDumper:
         self.objects_map = objects_map
 
     def map(self, obj, prop, value):
+        found = False
+        if f"{obj.__class__.__name__}.{prop}" in self.objects_map.keys():
+            target = self.objects_map[f"{obj.__class__.__name__}.{prop}"]
+            found = True
         if value.__class__.__name__ in self.objects_map.keys():
             target = self.objects_map[value.__class__.__name__]
+            found = True
+        if found:
             # If the object is mapped to None, discard the attribute and
             # continue to the next one.
             if target is None:
