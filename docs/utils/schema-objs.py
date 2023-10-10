9f338692e5be8fbdd9ed0b1b9c552546a4aa9f77
Rémi Palancher <remi@rackslab.io>
2023-10-10 14:07:49 +0200

    docs: simplify script to generate DB reference
    Now that objects and properties descriptions are fully integrated in the
    schema, there is no need for a special parser to extract comments. The
    description can simply be retrieved from shema objects and properties
    attributes.

--- docs/utils/schema-objs.py
+++ docs/utils/schema-objs.py
@@ -25,43 +25,10 @@
 """
 from pathlib import Path
 
-import yaml
 import jinja2
 
 from racksdb import RacksDB
 from racksdb.generic.schema import SchemaBackReference
-
-
-class LineLoader(yaml.loader.SafeLoader):
-    """Custom loader to line number of every nodes YAML file."""
-
-    def __init__(self, stream):
-        super().__init__(stream)
-
-    def compose_node(self, parent, index):
-        # the line number where the previous token has ended (plus empty lines)
-        line = self.line
-        node = yaml.composer.Composer.compose_node(self, parent, index)
-        node.__line__ = line
-        return node
-
-    def construct_mapping(self, node, deep=False):
-        additional_pairs = []
-
-        for key, value in node.value:
-            line_key = yaml.nodes.ScalarNode(
-                tag=yaml.resolver.BaseResolver.DEFAULT_SCALAR_TAG,
-                value="__line_" + key.value,
-            )
-            line_value = yaml.nodes.ScalarNode(
-                tag=yaml.resolver.BaseResolver.DEFAULT_SCALAR_TAG,
-                value=key.__line__,
-            )
-            additional_pairs.append((line_key, line_value))
-
-        node.value += additional_pairs
-        mapping = yaml.constructor.Constructor.construct_mapping(self, node, deep=deep)
-        return mapping
 
 
 def bases(obj):
@@ -75,34 +42,6 @@ def main():
         schema=Path("../schema/racksdb.yml"),
         db=Path("../examples/simple/racksdb.yml"),
     )
-
-    with open("../schema/racksdb.yml") as fh:
-        # Load the schema with LineLoader to get line numbers of attributes
-        # objects declarations
-        data = yaml.load(fh.read(), Loader=LineLoader)
-        # Also load schema file in memory to get individual lines content.
-        fh.seek(0)
-        schema_lines = fh.readlines()
-
-    # Enrich schema with description provided in schema file, in comment in the
-    # line of the property declaration.
-    for obj in [db._schema.content] + list(db._schema.objects.values()):
-        if obj.name != "_content":
-            obj_line = schema_lines[data["_objects"]["__line_" + obj.name]]
-            if "#" in obj_line:
-                obj.description = obj_line.split("#", 1)[1].strip()
-            else:
-                obj.description = None
-        for prop in obj.properties:
-            if obj is db._schema.content:
-                item = data["_content"]
-            else:
-                item = data["_objects"][obj.name]
-            prop_line = schema_lines[item["__line_" + prop.name]]
-            if "#" in prop_line:
-                prop.description = prop_line.split("#", 1)[1].strip()
-            else:
-                prop.description = "-"
 
     # Enrich schema object with has_backref boolean
     for obj in db._schema.objects.values():
