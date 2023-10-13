4ae155635b9115968770fc403daf54aceecc8a75
Florian Lespérant <florian.lesperant@rackslab.io>
2023-10-13 14:58:22 +0200

    feat: creation of the data table of a datacenter

--- racksdb/web/ui/src/views/DatacenterDetailsView.vue
+++ racksdb/web/ui/src/views/DatacenterDetailsView.vue
@@ -5,13 +5,38 @@ import type { Ref } from 'vue'
 
 const http = useHttp()
 const datacenters: Ref<Array<Datacenter>> = ref([])
+const racks: Ref<Array<Rack>> = ref([])
+const infrastructures: Ref<Array<Infrastructure>> = ref([])
 var showList =  ref(true)
 const input = ref('')
+var selectedRack: Ref<Rack | undefined> = ref(undefined)
+var selectedinfrastucture: Ref<Infrastructure | undefined> = ref(undefined)
+
 
 const props = defineProps({
     datacenterName: String
 })
 
+
+async function getInfrastructures(){
+    try{
+        const resp = await http.get('infrastructures')
+        infrastructures.value = resp.data as Infrastructure[]
+        selectedinfrastucture.value = infrastructures.value.find(infrastructure => infrastructure.rack === selectedRack.value?.name);
+    } catch (error) {
+       console.error('erreur', error)
+    }
+}
+
+async function getRacks(){
+    try{
+        const resp = await http.get('racks')
+        racks.value = resp.data as Rack[]
+        selectedRack.value = racks.value.find(rack => rack.datacenter === props.datacenterName);
+    } catch (error) {
+        console.error('erreur', error)
+    }
+}
 
 async function getDatacenters(){
     try {
@@ -47,6 +72,8 @@ function searchDatacenter() {
 
 onMounted(() => {
     getDatacenters()
+    getRacks()
+    getInfrastructures()
 })
 
 export interface Datacenter {
@@ -55,12 +82,27 @@ export interface Datacenter {
   show: boolean
 }
 
+export interface Infrastructure {
+    rack: string
+}
+
+export interface Rack {
+    name: string
+    datacenter: string
+    room: string
+    type : {
+        width: number
+        depth: number
+    }
+    row: string
+}
+
 </script>
 
 <template>
 
     <div>
-        <h1 class="text-5xl font-medium flex justify-center py-20 capitalize">{{ datacenterName }} Datacenter Details</h1>
+        <h1 class="text-5xl font-medium flex justify-center py-20 capitalize">Datacenter Details</h1>
 
         <div class="mx-60">
             <label for="search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
@@ -79,7 +121,78 @@ export interface Datacenter {
             <router-link v-for="datacenter in datacenters" :key="datacenter.name" :to="getDatacenterDetailsRoute(datacenter.name)">
               <li class="capitalize">{{datacenter.name}}</li></router-link>
         </ul> 
-
       
     </div>
+
+    
+<div class="relative overflow-x-auto py-20">
+    <h2 class="text-3xl font-medium flex justify-center capitalize py-16">{{ selectedRack?.datacenter }} Datacenter</h2>
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    Name
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Depth
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Width
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Number of racks
+                </th>
+                <th scope="col">
+                    Access to the room
+                </th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    {{ selectedRack?.room }}
+                </th>
+                <td class="px-6 py-4">
+                    {{ selectedRack?.type.depth }}
+                </td>
+                <td class="px-6 py-4">
+                    {{ selectedRack?.type.width }}
+                </td>
+                <td class="px-6 py-4">
+                    {{  selectedinfrastucture?.rack}}
+                </td>
+            </tr>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Microsoft Surface Pro
+                </th>
+                <td class="px-6 py-4">
+                    White
+                </td>
+                <td class="px-6 py-4">
+                    Laptop PC
+                </td>
+                <td class="px-6 py-4">
+                    $1999
+                </td>
+            </tr>
+            <tr class="bg-white dark:bg-gray-800">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Magic Mouse 2
+                </th>
+                <td class="px-6 py-4">
+                    Black
+                </td>
+                <td class="px-6 py-4">
+                    Accessories
+                </td>
+                <td class="px-6 py-4">
+                    $99
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
 </template>
