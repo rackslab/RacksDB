24ff231354f7bbcd594edeaa9a0d6aa8664a029b
Florian Lespérant <florian.lesperant@rackslab.io>
2023-10-17 15:25:34 +0200

    feat: creation of the searchbar using the component SearchBar

--- racksdb/web/ui/src/views/InfrastructuresView.vue
+++ racksdb/web/ui/src/views/InfrastructuresView.vue
@@ -1,23 +1,35 @@
 <script setup lang="ts">
+import { useHttp } from '@/plugins/http';
+import { ref, onMounted } from 'vue'
+import type { Ref } from 'vue'
+import SearchBarView from '@/components/SearchBarView.vue';
 
+var infrastructures: Ref<Array<Infrastructure>> = ref([])
+const http = useHttp()
+
+async function getInfrastructure(){
+    try {
+        const resp = await http.get('infrastructures')
+        infrastructures.value = resp.data as Infrastructure[]
+    } catch (error) {
+        console.error('Error during infrastructure data recovery', error)
+    }
+}
+
+onMounted(() => {
+    getInfrastructure()
+})
+
+export interface Infrastructure {
+  name: string
+}
 </script>
 
 <template>
-    <div>
-        <h1 class="text-5xl font-medium flex justify-center py-20">InfrastructuresView</h1>
-    
-        <div class="mx-60">
-            <label for="search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-                <div class="relative">
-                    <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                        </svg>
-                    </div>
-                    <input type="search" id="search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search an infrastructure" required>
-                    <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
-                </div>
-        </div>
-
-    </div>
+    <SearchBarView 
+        v-if="infrastructures.length"
+        viewTitle="Infrastructure View"    
+        searchedItem="infrastructure" 
+        :items="infrastructures"
+    />
 </template>
