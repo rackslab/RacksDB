c5bc84d1a4a262535c9ac598e4649c6b6965615d
Florian Lespérant <florian.lesperant@rackslab.io>
2023-10-24 13:47:02 +0200

    feat: creation of the popup

--- racksdb/web/ui/src/components/ContentCard.vue
+++ racksdb/web/ui/src/components/ContentCard.vue
@@ -1,17 +1,59 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import type { Ref } from 'vue'
+import type { Infrastructure, NodeEquipment, NetworkEquipment, StorageEquipment } from './InfrastructureCards.vue';
+
+var showPopUp = ref(false)
+var popUpContent: Ref<NodeEquipment | NetworkEquipment | StorageEquipment | undefined > = ref()
+
+
     
-defineProps({
+const props = defineProps({
     rack: String,
     tag: String,
-    name: String,
-    id: String
+    name: {
+        type: String,
+        required: true
+    },
+    id: String,
+    items: {
+        type: Array<Infrastructure>,
+            default() {
+                return []
+            }
+    }
 })
+        
+function popUp(name: string){
+    if(showPopUp.value){
+        showPopUp.value = !showPopUp.value
+    } else {
+        for(var i=0; i < props.items.length; i++){
+            var layout = props.items[i].layout
+
+            for(var y=0; y < layout.length; y++){
+                if (layout[y].nodes.find(node => node.name === name)){
+                    popUpContent.value = layout[y].nodes.find(node => node.name === name)
+                    break
+                } 
+
+
+
+            }
+
+        }
+        showPopUp.value = true      
+
+
+    }
+}
+
 
 </script>
 
 <template>
 
-<div class="flex justify-around pt-32 px-32 ">
+<div class="flex justify-around pt-32 px-32" @click="popUp(name)">
     <div class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
         <h2 class="text-2xl font-semibold flex justify-center text-purple-700 capitalize"> {{ rack }} </h2>
             <h3 class="flex justify-center text-purple-700 capitalize">{{ tag }}</h3>
@@ -26,5 +68,34 @@ defineProps({
                     </li>
                 </ul>
     </div>
+
+    <div v-if="showPopUp && popUpContent">
+        <div v-if="'cpu' in popUpContent['type']">
+            <p class="font-medium">ID: {{ popUpContent.type.id }}</p>
+            <p>Model: {{ popUpContent.type.model }}</p>
+            <p>Height: {{ popUpContent.type.height }}</p>
+            <p>Width: {{ popUpContent.type.width }}</p>
+            <p>Specs: {{ popUpContent.type.specs }}</p>
+
+            <p class="font-medium">CPU:</p>
+            <p>Sockets: {{ popUpContent.type.cpu.sockets }}</p>
+            <p>Model: {{ popUpContent.type.cpu.model }}</p>
+            <p>Specs: {{ popUpContent.type.cpu.specs }}</p>
+            <p>Cores: {{ popUpContent.type.cpu.cores }}</p>
+
+            <p class="font-medium">RAM:</p>
+            <p>Dimm: {{ popUpContent.type.ram.dimm }}</p>
+            <p>Size: {{ popUpContent.type.ram.size / (1024**3) }}GB</p>
+        </div>
+
+        <div v-else-if="'disks' in popUpContent['type']">
+            <p>{{ popUpContent.name}}</p>
+
+        </div>
+        
+    </div>
+
+    
+
 </div>
 </template>
