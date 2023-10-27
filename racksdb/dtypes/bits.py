7dedf46c9664db0bdfed28fb8194d054f2beb097
Rémi Palancher <remi@rackslab.io>
2023-10-27 18:14:40 +0200

    feat(core): introduce bits defined type

--- racksdb/dtypes/bits.py
+++ racksdb/dtypes/bits.py
@@ -7,19 +7,19 @@
 from racksdb.generic.definedtype import SchemaDefinedType
 
 
-class SchemaDefinedTypeBytes(SchemaDefinedType):
+class SchemaDefinedTypeBits(SchemaDefinedType):
 
-    pattern = r"(\d+(.\d+)?)(TB|GB|MB)"
+    pattern = r"(\d+(.\d+)?)(Tb|Gb|Mb)"
     native = int
 
     def parse(self, value):
         match = self._match(value)
         quantity = float(match.group(1))
         unit = match.group(3)
-        if unit == "MB":
-            quantity *= 1024 ** 2
-        elif unit == "GB":
-            quantity *= 1024 ** 3
-        elif unit == "TB":
-            quantity *= 1024 ** 4
+        if unit == "Mb":
+            quantity *= (10 ** 6)
+        elif unit == "Gb":
+            quantity *= (10 ** 9)
+        elif unit == "Tb":
+            quantity *= (10 ** 12)
         return int(quantity)
