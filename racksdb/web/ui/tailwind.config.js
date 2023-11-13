a79d891259d7a32d3f70bc522fd267d3faed2d85
Florian Lespérant <florian.lesperant@rackslab.io>
2023-11-13 16:25:40 +0100

    fix: add eslint hint in nodejs files

--- racksdb/web/ui/tailwind.config.js
+++ racksdb/web/ui/tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+/* eslint-env node */
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
@@ -10,4 +11,3 @@ module.exports = {
   },
   plugins: [],
 }
-
