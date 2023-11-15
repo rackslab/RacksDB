56a006d6405b1fd2d11af01cfe88bbba96c44fee
Florian Lespérant <florian.lesperant@rackslab.io>
2023-11-15 07:51:55 +0100

    fix: use names instead of the paths in router links

--- racksdb/web/ui/src/components/HeaderPage.vue
+++ racksdb/web/ui/src/components/HeaderPage.vue
@@ -5,51 +5,42 @@ This file is part of RacksDB.
 SPDX-License-Identifier: GPL-3.0-or-later -->
 
 <script setup lang="ts">
-import { RouterLink, RouterView, useRouter } from 'vue-router'
+import { RouterLink, useRouter } from 'vue-router'
 
 const router = useRouter()
 
-function isRouteActive(routePath: string) {
-  return router.currentRoute.value.path === routePath
-}
-
-function isDatacentersRouteActive() {
-  return router.currentRoute.value.path.startsWith('/datacenters')
-}
-
-function isInfrastructuresRouteActive() {
-  return router.currentRoute.value.path.startsWith('/infrastructures')
+function isRouteActive(route: string) {
+  return router.currentRoute.value.path === route
 }
 </script>
 
 <template>
   <header class="flex justify-between items-center p-4 px-20">
     <div>
-      <router-link to="/"
+      <router-link :to="{name: 'home'}"
         ><img alt="Rackslab logo" src="/assets/logo_racksdb.svg" class="h-32 w-32"
       /></router-link>
     </div>
 
     <nav class="space-x-4">
-      <router-link to="/">
+      <router-link :to="{name: 'home'}">
         <span v-if="isRouteActive('/')" class="text-purple-700 font-medium">Home</span>
         <span v-else>Home</span>
       </router-link>
 
-      <router-link to="/datacenters">
-        <span v-if="isDatacentersRouteActive()" class="text-purple-700 font-medium"
+      <router-link :to="{name:'datacenters'}">
+        <span v-if="isRouteActive('datacenters')" class="text-purple-700 font-medium"
           >Datacenters</span
         >
         <span v-else>Datacenters</span>
       </router-link>
 
-      <router-link to="/infrastructures">
-        <span v-if="isInfrastructuresRouteActive()" class="text-purple-700 font-medium"
+      <router-link :to="{name: 'infrastructures'}">
+        <span v-if="isRouteActive('infrastructures')" class="text-purple-700 font-medium"
           >Infrastructures</span
         >
         <span v-else>Infrastructures</span>
       </router-link>
     </nav>
   </header>
-  <RouterView />
 </template>
