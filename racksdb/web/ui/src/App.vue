9de19fa7ce0a0b435be68ce50630f9742716fa13
Florian Lespérant <florian.lesperant@rackslab.io>
2023-11-15 07:53:21 +0100

    feat: add the RouterView in App.vue instead of putting it in HeaderPage component

--- racksdb/web/ui/src/App.vue
+++ racksdb/web/ui/src/App.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
 import HeaderPage from './components/HeaderPage.vue'
+import { RouterView } from 'vue-router'
+
 </script>
 
 <template>
   <HeaderPage />
+  <RouterView />
 </template>
