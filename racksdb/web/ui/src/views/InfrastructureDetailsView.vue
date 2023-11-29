075f4136861c0a608b17706748c511ae3aebfbbe
Florian Lespérant <florian.lesperant@rackslab.io>
2023-11-29 09:43:12 +0100

    fix: use icons from heroicons + delete the code to display the data in table form use instead the component InfrastructureTable

--- racksdb/web/ui/src/views/InfrastructureDetailsView.vue
+++ racksdb/web/ui/src/views/InfrastructureDetailsView.vue
@@ -9,39 +9,25 @@ import { useHttp } from '@/plugins/http'
 import { ref, onMounted, inject } from 'vue'
 import SearchBar from '@/components/SearchBar.vue'
 import InfrastructureCards from '@/components/InfrastructureCards.vue'
+import InfrastructureTable from '@/components/InfrastructureTable.vue'
 import { injectionKey } from '@/plugins/runtimeConfiguration'
 import type { Ref } from 'vue'
 import type { Infrastructure } from '@/views/InfrastructuresView.vue'
-import {
-  Squares2X2Icon,
-  TableCellsIcon
-} from '@heroicons/vue/24/outline'
+import { Squares2X2Icon, TableCellsIcon } from '@heroicons/vue/24/outline'
 
 const http = useHttp()
 const infrastructures: Ref<Array<Infrastructure>> = ref([])
 const infrastructureDetails: Ref<Infrastructure | undefined> = ref()
 const showFullImg = ref(false)
-const rack = ref()
 const cardsView = ref(true)
-const showRack = ref(false)
-const showInfrastructureRacks = ref(true)
 
-function changeView(){
-    cardsView.value = false
+// this function changes the display by assigning the opposite value to cardsView
+function changeView() {
+  cardsView.value = !cardsView.value
 }
 
-function openImg() {
+function toggleImageModal() {
   showFullImg.value = !showFullImg.value
-}
-
-function rackDetails(rackName: string) {
-  showRack.value = true
-  showInfrastructureRacks.value = false
-  var layout = infrastructureDetails.value?.layout
-
-  for (let index = 0; index < layout!.length; index++) {
-    rack.value = layout!.filter((rack) => rack.rack === rackName)
-  }
 }
 
 async function getInfrastructure() {
@@ -80,7 +66,7 @@ const props = defineProps({
   <img
     :src="`${inject(injectionKey)!.api_server}/draw/infrastructure/${props.name}.svg`"
     alt=""
-    @click="openImg()"
+    @click="toggleImageModal()"
     class="h-96 max-w-500 mx-auto p-10"
   />
 
@@ -91,20 +77,20 @@ const props = defineProps({
     <img
       :src="`${inject(injectionKey)!.api_server}/draw/infrastructure/${props.name}.svg`"
       alt=""
-      @click="openImg()"
+      @click="toggleImageModal()"
       class="h-screen max-w-full bg-white"
     />
   </div>
 
   <div class="flex justify-center pb-5">
     <div v-if="cardsView" class="flex">
-      <Squares2X2Icon @click="changeView()" class="h-20 w-20"/>
-      <TableCellsIcon @click="changeView()" class="h-10 w-10"/>
+      <Squares2X2Icon @click="changeView()" class="h-20 w-20" />
+      <TableCellsIcon @click="changeView()" class="h-10 w-10" />
     </div>
 
     <div v-if="!cardsView" class="flex">
-      <Squares2X2Icon @click="changeView()" class="h-10 w-10"/>
-      <TableCellsIcon @click="changeView()" class="h-20 w-20"/>
+      <Squares2X2Icon @click="changeView()" class="h-10 w-10" />
+      <TableCellsIcon @click="changeView()" class="h-20 w-20" />
     </div>
   </div>
 
@@ -127,118 +113,10 @@ const props = defineProps({
   </div>
 
   <div v-show="!cardsView" class="pb-10">
-    <div
-      class="flex justify-center pb-28"
-      v-for="infrastructure in infrastructures"
-      :key="infrastructure.name"
-    >
-      <div v-for="rack in infrastructure.layout" :key="rack.rack">
-        <button
-          type="button"
-          @click="rackDetails(rack.rack)"
-          class="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-        >
-          {{ rack.rack }}
-        </button>
-      </div>
-    </div>
-
-    <div v-show="showInfrastructureRacks">
-      <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead
-          class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
-        >
-          <tr>
-            <th scope="col" class="px-6 py-3">Name</th>
-            <th scope="col" class="px-6 py-3">Equipment</th>
-            <th scope="col" class="px-6 py-3">ID</th>
-          </tr>
-        </thead>
-
-        <template v-for="layout in infrastructureDetails?.layout" :key="layout.rack">
-          <tr v-for="node in layout.nodes" :key="node.name">
-            <td
-              scope="row"
-              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white capitalize"
-            >
-              {{ node.name }}
-            </td>
-            <td class="px-6 py-4">Node {{ node.rack }}</td>
-            <td class="px-6 py-4 capitalize">{{ node.type.id }}</td>
-          </tr>
-
-          <tr v-for="storage in layout.storage" :key="storage.name">
-            <td
-              scope="row"
-              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white capitalize"
-            >
-              {{ storage.name }}
-            </td>
-            <td class="px-6 py-4">Storage {{ storage.rack }}</td>
-            <td class="px-6 py-4 capitalize">{{ storage.type.id }}</td>
-          </tr>
-
-          <tr v-for="network in layout.network" :key="network.name">
-            <td
-              scope="row"
-              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white capitalize"
-            >
-              {{ network.name }}
-            </td>
-            <td class="px-6 py-4">Network {{ network.rack }}</td>
-            <td class="px-6 py-4 capitalize">{{ network.type.id }}</td>
-          </tr>
-        </template>
-      </table>
-    </div>
-
-    <div v-show="showRack">
-      <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead
-          class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
-        >
-          <tr>
-            <th scope="col" class="px-6 py-3">Name</th>
-            <th scope="col" class="px-6 py-3">Equipment</th>
-            <th scope="col" class="px-6 py-3">ID</th>
-          </tr>
-        </thead>
-
-        <template v-for="layout in rack" :key="layout.rack">
-          <tr v-for="node in layout.nodes" :key="node.name">
-            <td
-              scope="row"
-              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white capitalize"
-            >
-              {{ node.name }}
-            </td>
-            <td class="px-6 py-4">Node {{ node.rack }}</td>
-            <td class="px-6 py-4 capitalize">{{ node.type.id }}</td>
-          </tr>
-
-          <tr v-for="storage in layout.storage" :key="storage.name">
-            <td
-              scope="row"
-              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white capitalize"
-            >
-              {{ storage.name }}
-            </td>
-            <td class="px-6 py-4">Storage {{ storage.rack }}</td>
-            <td class="px-6 py-4 capitalize">{{ storage.type.id }}</td>
-          </tr>
-
-          <tr v-for="network in layout.network" :key="network.name">
-            <td
-              scope="row"
-              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white capitalize"
-            >
-              {{ network.name }}
-            </td>
-            <td class="px-6 py-4">Network {{ network.rack }}</td>
-            <td class="px-6 py-4 capitalize">{{ network.type.id }}</td>
-          </tr>
-        </template>
-      </table>
-    </div>
+    <InfrastructureTable
+      v-if="infrastructureDetails"
+      :infrastructure="infrastructures"
+      :infrastructureDetails="infrastructureDetails"
+    />
   </div>
 </template>
