cc01890d92cd7d3ddfdcd9e678bebff82822079b
Rémi Palancher <remi@rackslab.io>
2024-03-08 10:57:09 +0100

    fix(ui): remove useless theme extension

--- racksdb/web/ui/tailwind.config.js
+++ racksdb/web/ui/tailwind.config.js
@@ -2,13 +2,6 @@
 /* eslint-env node */
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'racks_black': "url('/assets/racks_black.jpg')"
-      }
-    },
-  },
   plugins: [
     require('@tailwindcss/forms'),
   ],
