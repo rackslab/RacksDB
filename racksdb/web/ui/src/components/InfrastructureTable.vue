1c3e587dd7050e946b73c93a6cf84ef45760e7dd
Florian Lespérant <florian.lesperant@rackslab.io>
2024-03-11 15:48:14 +0100

    feat(ui): Add the slider

--- racksdb/web/ui/src/components/InfrastructureTable.vue
+++ racksdb/web/ui/src/components/InfrastructureTable.vue
@@ -22,9 +22,12 @@ import {
   BarsArrowDownIcon,
   BarsArrowUpIcon,
   ChevronDownIcon,
-  ChevronUpIcon
+  ChevronUpIcon,
+  XMarkIcon
 } from '@heroicons/vue/24/outline'
 import EquipmentTypeModal from '@/components/EquipmentTypeModal.vue'
+import { FunnelIcon } from '@heroicons/vue/24/solid'
+import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot } from '@headlessui/vue'
 
 var showModal = ref(false)
 var modalContent: Ref<
@@ -32,6 +35,7 @@ var modalContent: Ref<
 > = ref()
 const alphabeticalOrder = ref(true)
 const displayRacks: Ref<Record<string, boolean>> = ref({})
+const showSlider = ref(false)
 
 type EquipmentType = 'nodes' | 'storage' | 'network' | 'misc'
 
@@ -49,6 +53,10 @@ interface Storage extends StorageEquipment {
 
 interface Misc extends MiscEquipment {
   equipmentType: EquipmentType
+}
+
+function toggleSlider() {
+  showSlider.value = !showSlider.value
 }
 
 function displayRackEquipment(rackName: string) {
@@ -148,8 +156,93 @@ onMounted(() => {
         <BarsArrowUpIcon v-else class="h-7 w-7" />
         <p class="pl-2">Sort racks</p>
       </div>
+      <div
+        class="flex pt-5 cursor-pointer hover:text-purple-700 hover:duration-100 duration-150"
+        @click="toggleSlider()"
+      >
+        <p class="pr-2">Filters</p>
+        <FunnelIcon class="h-7 w-7" />
+      </div>
     </div>
   </div>
+
+  <!-- Slider -->
+  <template>
+    <TransitionRoot as="template" :show="showSlider">
+      <Dialog as="div" class="relative z-10" @close="showSlider = false">
+        <TransitionChild
+          as="template"
+          enter="ease-in-out duration-500"
+          enter-from="opacity-0"
+          enter-to="opacity-100"
+          leave="ease-in-out duration-500"
+          leave-from="opacity-100"
+          leave-to="opacity-0"
+        >
+          <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </TransitionChild>
+
+        <div class="fixed inset-0 overflow-hidden">
+          <div class="absolute inset-0 overflow-hidden">
+            <div class="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+              <TransitionChild
+                as="template"
+                enter="transform transition ease-in-out duration-500 sm:duration-700"
+                enter-from="translate-x-full"
+                enter-to="translate-x-0"
+                leave="transform transition ease-in-out duration-500 sm:duration-700"
+                leave-from="translate-x-0"
+                leave-to="translate-x-full"
+              >
+                <DialogPanel class="pointer-events-auto relative w-screen max-w-md">
+                  <TransitionChild
+                    as="template"
+                    enter="ease-in-out duration-500"
+                    enter-from="opacity-0"
+                    enter-to="opacity-100"
+                    leave="ease-in-out duration-500"
+                    leave-from="opacity-100"
+                    leave-to="opacity-0"
+                  >
+                    <div class="absolute left-0 top-0 -ml-8 flex pr-2 pt-4 sm:-ml-10 sm:pr-4">
+                      <button
+                        type="button"
+                        class="relative rounded-md text-gray-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-white"
+                        @click="showSlider = false"
+                      >
+                        <span class="absolute -inset-2.5" />
+                        <span class="sr-only">Close panel</span>
+                        <XMarkIcon class="h-6 w-6" aria-hidden="true" />
+                      </button>
+                    </div>
+                  </TransitionChild>
+                  <div class="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
+                    <div class="px-4 sm:px-6">
+                      <DialogTitle class="text-lg font-semibold leading-6 text-gray-900"
+                        >Filters</DialogTitle
+                      >
+                    </div>
+                    <div class="relative mt-6 flex-1 px-4 sm:px-6">
+                      <!-- Racks -->
+
+                      <!-- Equipment category -->
+
+                      <!-- Equipment types -->
+
+                      <!-- Tags -->
+
+                      <!-- Equipment name -->
+                    </div>
+                  </div>
+                </DialogPanel>
+              </TransitionChild>
+            </div>
+          </div>
+        </div>
+      </Dialog>
+    </TransitionRoot>
+  </template>
+
   <div class="flex justify-center my-auto mx-auto pt-5">
     <table class="min-w-[60vw] text-center text-gray-500 dark:text-gray-400 table-fixed">
       <thead
