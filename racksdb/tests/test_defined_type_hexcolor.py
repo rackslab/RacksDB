07c0e3f414d2e34d243eb3c894655e7bad8fc7ff
Rémi Palancher <remi@rackslab.io>
2024-03-11 09:01:54 +0100

    tests: fix hexcolor defined type test
    It has not been updated after alpha channel introduction.

--- racksdb/tests/test_defined_type_hexcolor.py
+++ racksdb/tests/test_defined_type_hexcolor.py
@@ -13,8 +13,10 @@ from racksdb.generic.errors import DBFormatError
 class TestDefinedTypeHexcolor(unittest.TestCase):
     def test_defined_type_hexcolor(self):
         defined_type = SchemaDefinedTypeHexcolor()
-        self.assertEqual(defined_type.parse("#004080"), (0, 64 / 255, 128 / 255))
-        self.assertEqual(defined_type.parse("#ffb4c5"), (1.0, 180 / 255, 197 / 255))
+        self.assertEqual(defined_type.parse("#004080"), (0, 64 / 255, 128 / 255, 1.0))
+        self.assertEqual(
+            defined_type.parse("#ffb4c5"), (1.0, 180 / 255, 197 / 255, 1.0)
+        )
 
     def test_defined_type_hexcolor_invalid_values(self):
         defined_type = SchemaDefinedTypeHexcolor()
