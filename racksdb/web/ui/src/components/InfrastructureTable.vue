60afd4916e1751ca5456be92a25861dfee8c0479
Florian Lespérant <florian.lesperant@rackslab.io>
2024-03-12 07:57:06 +0100

    feat(ui): Add the filter 'racks'

--- racksdb/web/ui/src/components/InfrastructureTable.vue
+++ racksdb/web/ui/src/components/InfrastructureTable.vue
@@ -17,17 +17,27 @@ import type {
   StorageEquipmentType,
   MiscEquipmentType
 } from '@/composables/RacksDBAPI'
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, watch } from 'vue'
 import {
   BarsArrowDownIcon,
   BarsArrowUpIcon,
   ChevronDownIcon,
   ChevronUpIcon,
-  XMarkIcon
+  XMarkIcon,
+  ChevronUpDownIcon,
+  CheckIcon
 } from '@heroicons/vue/24/outline'
 import EquipmentTypeModal from '@/components/EquipmentTypeModal.vue'
 import { FunnelIcon } from '@heroicons/vue/24/solid'
 import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot } from '@headlessui/vue'
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxOption,
+  ComboboxOptions,
+  ComboboxLabel,
+  ComboboxButton
+} from '@headlessui/vue'
 
 var showModal = ref(false)
 var modalContent: Ref<
@@ -36,6 +46,8 @@ var modalContent: Ref<
 const alphabeticalOrder = ref(true)
 const displayRacks: Ref<Record<string, boolean>> = ref({})
 const showSlider = ref(false)
+const selectedRacks: Ref<Array<string>> = ref([])
+const infrastructureRacks: Ref<Array<string>> = ref([])
 
 type EquipmentType = 'nodes' | 'storage' | 'network' | 'misc'
 
@@ -141,8 +153,29 @@ const props = defineProps({
 onMounted(() => {
   props.infrastructureDetails.layout.forEach((rack) => {
     displayRacks.value[rack.rack] = true
+    infrastructureRacks.value.push(rack.rack)
   })
 })
+
+watch(
+  () => selectedRacks.value,
+  () => {
+    const filteredRacks: Record<string, boolean> = {}
+    if (selectedRacks.value.length > 0) {
+      selectedRacks.value.forEach((rack) => {
+        filteredRacks[rack] = true
+      })
+
+      displayRacks.value = filteredRacks
+    } else {
+      infrastructureRacks.value.forEach((rack) => {
+        filteredRacks[rack] = true
+      })
+
+      displayRacks.value = filteredRacks
+    }
+  }
+)
 </script>
 
 <template>
@@ -224,6 +257,66 @@ onMounted(() => {
                     </div>
                     <div class="relative mt-6 flex-1 px-4 sm:px-6">
                       <!-- Racks -->
+                      <Combobox as="div" v-model="selectedRacks" multiple class="p-4">
+                        <ComboboxLabel class="text-lg">Racks</ComboboxLabel>
+
+                        <div
+                          class="pt-3 relative w-full cursor-default rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm"
+                        >
+                          <ComboboxInput
+                            class="w-96 capitalize focus:border-purple-700 focus:outline-none border-2 border-solid rounded-md bg-white py-1.5 pl-3 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-purple-600 sm:text-sm sm:leading-6"
+                            :value="selectedRacks"
+                          />
+                          <ComboboxButton class="pt-3 absolute inset-y-0 right-0 flex items-center">
+                            <ChevronUpDownIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+                          </ComboboxButton>
+                        </div>
+                        <transition
+                          enter-active-class="transition duration-100 ease-out"
+                          enter-from-class="transform scale-95 opacity-0"
+                          enter-to-class="transform scale-100 opacity-100"
+                          leave-active-class="transition duration-75 ease-out"
+                          leave-from-class="transform scale-100 opacity-100"
+                          leave-to-class="transform scale-95 opacity-0"
+                        >
+                          <ComboboxOptions
+                            class="w-96 mt-1 max-h-60 overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+                          >
+                            <ComboboxOption
+                              v-for="rack in infrastructureRacks"
+                              :key="rack"
+                              :value="rack"
+                              v-slot="{ active, selected }"
+                            >
+                              <li
+                                :class="[
+                                  'relative cursor-default select-none py-2 pl-3 pr-9',
+                                  active ? 'bg-purple-600 text-white' : 'text-gray-900'
+                                ]"
+                              >
+                                <span
+                                  :class="[
+                                    'block truncate capitalize',
+                                    selected && 'font-semibold'
+                                  ]"
+                                >
+                                  {{ rack }}
+                                </span>
+
+                                <span
+                                  v-if="selected"
+                                  :class="[
+                                    'absolute inset-y-0 right-0 flex items-center pr-4',
+                                    active ? 'text-white' : 'text-purple-600'
+                                  ]"
+                                >
+                                  <CheckIcon class="h-5 w-5" aria-hidden="true" />
+                                </span>
+                              </li>
+                            </ComboboxOption>
+                          </ComboboxOptions>
+                        </transition>
+                      </Combobox>
 
                       <!-- Equipment category -->
 
