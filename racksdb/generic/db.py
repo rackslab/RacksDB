rackslab/RacksDB

commit 9173a77bc2a2a46342322133ceeca634a21ecd7c

fix(core): folded equipment name filtering
DBDicts of equipment were not properly filtered by name when folded
because they were not expanded before filtering. This is fixed now by
expanding the folded object prior to calling the _filter method.

fix #73

racksdb/generic/db.py
@@ -205,9 +205,17 @@ class DBDict(dict):
         """Return a copy of the current DBDict without key and values that do not match
         provided filter criteria."""
         result = DBDict()
-        for key, value in self.items():
+
+        def add_match(key, value):
             if value._filter(**kwargs):
                 result[key] = value
+
+        for key, value in self.items():
+            if isinstance(value, DBExpandableObject):
+                for _expanded_value in value.objects():
+                    add_match(key, _expanded_value)
+            else:
+                add_match(key, value)
         return result
 
     def __iter__(self):

commit 3337bd53f962e41dca7b0ae2513052ed24e28b3a

fix(core): filtered folded values same DBDict key
Fix regression introduced in 9173a77 where expanded values after
filtering are all colliding because associated to the same key in
resulting DBDict. Use the rangeset value at iteration index to get
distinctive keys for all expanded values.

racksdb/generic/db.py
@@ -206,8 +206,8 @@ class DBDict(dict):
 
         for key, value in self.items():
             if isinstance(value, DBExpandableObject):
-                for _expanded_value in value.objects():
-                    add_match(key, _expanded_value)
+                for idx, _expanded_value in enumerate(value.objects()):
+                    add_match(key.rangeset[idx], _expanded_value)
             else:
                 add_match(key, value)
         return result