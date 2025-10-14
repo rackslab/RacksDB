fd14f0d251796e7839c5a5c7939dbfd9ae0ed957
Rémi Palancher <remi@rackslab.io>
2025-10-14 21:46:59 +0200

    tests: add missing header in source file

--- frontend/tests/components/DatacenterListInfrastructures.spec.ts
+++ frontend/tests/components/DatacenterListInfrastructures.spec.ts
@@ -1,3 +1,11 @@
+/*
+Copyright (c) 2025 Rackslab
+
+This file is part of RacksDB.
+
+SPDX-License-Identifier: GPL-3.0-or-later
+*/
+
 import { describe, test, expect } from 'vitest'
 import { mount } from '@vue/test-utils'
 
